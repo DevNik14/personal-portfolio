8ef0381be9bcb7b1aea54d12009082a34d8dbb4c
DevNik14 <nykyurchiyski@gmail.com>
2023-08-23 20:21:27 +0300

    add basic page transitions with gsap

--- middlewares/renderMiddleware.js
+++ middlewares/renderMiddleware.js
@@ -1,10 +1,24 @@
 import { html, render } from 'lit-html';
+import { gsap } from 'gsap';
 
 const root = document.querySelector('#root');
 
 const renderLayout = (ctx, template) => {
-  render(template, root);
-}
+  gsap.to(root, {
+    opacity: 0, 
+    x: '-100%', 
+    duration: 0.5,
+    onComplete: () => {
+      render(template, root); 
+      gsap.set(root, { x: '100%', opacity: 0 }); 
+      gsap.to(root, {
+        x: '0%', 
+        opacity: 1, 
+        duration: 0.5,
+      });
+    },
+  });
+};
 
 export const renderMiddleware = (ctx, next) => {
   ctx.render = renderLayout.bind(null, ctx);
