25c487c7f27d3529cd754f5f020b4d7a54a2faa0
DevNik14 <nykyurchiyski@gmail.com>
2023-08-23 13:44:14 +0300

    add mock views for projects and contact page

--- src/app.js
+++ src/app.js
@@ -7,13 +7,22 @@ const homeTemplate = () => html`<h1 class="heading heading-text">Home</h1>`
 
 const homeView = () => render(homeTemplate(), root);
 
-const aboutTemplate = () => html`<h1>About</h1>`
+const aboutTemplate = () => html`<h1 class="heading heading-text">About</h1>`
 
 const aboutView = () => render(aboutTemplate(), root);
+
+const projectsTemplate = () => html`<h1 class="heading heading-text">Projects</h1>`
+
+const projectsView = () => render(projectsTemplate(), root);
+
+const contactTemplate = () => html`<h1 class="heading heading-text">Contact</h1>`
+
+const contactView = () => render(contactTemplate(), root);
 
 
 page('/', homeView);
 page('/about', aboutView);
-page('/', homeView);
+page('/projects', projectsView);
+page('/contact', contactView);
 
 page.start();
