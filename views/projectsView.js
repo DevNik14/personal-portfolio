5475f2bc10ca52134b103c9854490b11b64859c4
DevNik14 <nykyurchiyski@gmail.com>
2023-08-24 21:30:16 +0300

    add mock projects and style the projects page

--- views/projectsView.js
+++ views/projectsView.js
@@ -1,6 +1,27 @@
 import {render, html} from 'lit-html';
 
-const projectsTemplate = () => html`<h1 class="heading heading-text">Projects</h1>`
+const projectsTemplate = () => html`
+  <main class="main-projects">
+    <h1 class="heading heading-text">Projects</h1>
+    <section class="projects-container">
+      <div class="project">
+          <div class="image"><span class="text">This is a project</span></div>
+          <span class="title">Cool Project</span>
+          <span class="price">$100</span>
+      </div>
+      <div class="project">
+          <div class="image"><span class="text">This is a project.</span></div>
+          <span class="title">Cool Project</span>
+          <span class="price">$200</span>
+      </div>
+      <div class="project">
+          <div class="image"><span class="text">This is a project.</span></div>
+          <span class="title">Cool Project</span>
+          <span class="price">$300</span>
+      </div>
+    </section>
+  </main>
+`
 
 export const projectsView = (ctx, next) => {
   ctx.render(projectsTemplate());
