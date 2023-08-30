c4615398416cb272fdfc9be03f31c495c0cd6cb9
DevNik14 <nykyurchiyski@gmail.com>
2023-08-30 14:55:13 +0300

    add styling and dummy text for the project description card

--- views/projectsView.js
+++ views/projectsView.js
@@ -5,19 +5,34 @@ const projectsTemplate = () => html`
     <h1 class="heading heading-text projects-heading">Projects</h1>
     <section class="projects-container">
       <div class="project">
-          <div class="image"><span class="text">This is a project</span></div>
-          <span class="title">Cool Project</span>
-          <span class="price">$100</span>
+        <div class="project-description">
+          <h3 class="project-title heading-text">Project Title</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <span class="price">$100</span>
       </div>
       <div class="project">
-          <div class="image"><span class="text">This is a project.</span></div>
-          <span class="title">Cool Project</span>
-          <span class="price">$200</span>
+        <div class="project-description">
+          <h3 class="project-title heading-text">Project Title</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <span class="price">$200</span>
       </div>
       <div class="project">
-          <div class="image"><span class="text">This is a project.</span></div>
-          <span class="title">Cool Project</span>
-          <span class="price">$300</span>
+        <div class="project-description">
+          <h3 class="project-title heading-text">Project Title</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <span class="price">$300</span>
       </div>
     </section>
   </main>
