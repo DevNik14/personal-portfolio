1fc04b5492f26798fbd8743db88a351336785f21
DevNik14 <nykyurchiyski@gmail.com>
2023-08-30 17:32:23 +0300

    add links for the project

--- views/projectsView.js
+++ views/projectsView.js
@@ -18,21 +18,17 @@ const projectsTemplate = () => html`
             <li class="project-item">HTML</li>
             <li class="project-item">HTML</li>
           </ul>
-        </footer>
-      </div>
-      <div class="project">
-        <div class="project-description">
-          <h3 class="project-title heading-text">Project Title</h3>
-          <p class="body-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          </p>
-        </div>
-        <footer class="project-footer">
-          <ul class="project-tools-list body-text">
-            <li class="project-item">HTML</li>
-            <li class="project-item">HTML</li>
-            <li class="project-item">HTML</li>
+          <ul class="links">
+            <li class="link-item">
+              <a href="github.com" target="_blank">
+                <i class="fa fa-github" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li class="link-item">
+              <a href="github.com" target="_blank">
+                <i class="fa fa-external-link" aria-hidden="true"></i>
+              </a>
+            </li>
           </ul>
         </footer>
       </div>
@@ -49,6 +45,46 @@ const projectsTemplate = () => html`
             <li class="project-item">HTML</li>
             <li class="project-item">HTML</li>
             <li class="project-item">HTML</li>
+          </ul>
+          <ul class="links">
+            <li class="link-item">
+              <a href="github.com" target="_blank">
+                <i class="fa fa-github" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li class="link-item">
+              <a href="github.com" target="_blank">
+                <i class="fa fa-external-link" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </div>
+      <div class="project">
+        <div class="project-description">
+          <h3 class="project-title heading-text">Project Title</h3>
+          <p class="body-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <footer class="project-footer">
+          <ul class="project-tools-list body-text">
+            <li class="project-item">HTML</li>
+            <li class="project-item">HTML</li>
+            <li class="project-item">HTML</li>
+          </ul>
+          <ul class="links">
+            <li class="link-item">
+              <a href="github.com" target="_blank">
+                <i class="fa fa-github" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li class="link-item">
+              <a href="github.com" target="_blank">
+                <i class="fa fa-external-link" aria-hidden="true"></i>
+              </a>
+            </li>
           </ul>
         </footer>
       </div>
