8728a6aa3d0d6c54d212024158891b55e5c4771f
DevNik14 <nykyurchiyski@gmail.com>
2024-01-01 01:06:40 +0200

    remove react-roter and install react-scroll

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,13 @@
-import { Routes, Route, Link } from 'react-router-dom'
-
 const Navigation = () => {
   return (
     <>
-      <aside>
+      <aside className="sidebar">
         <nav className="site-navigation">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/contacts">Contacts</Link></li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contacts</li>
         </ul>
         </nav> 
       </aside>
