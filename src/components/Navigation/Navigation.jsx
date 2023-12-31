033e0f87e1b1c3abbb950372ac8230bba13f3eca
DevNik14 <nykyurchiyski@gmail.com>
2024-01-01 00:22:28 +0200

    add global styles and nav element to the navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,12 +4,14 @@ const Navigation = () => {
   return (
     <>
       <aside>
+        <nav className="site-navigation">
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/projects">Projects</Link></li>
           <li><Link to="/contacts">Contacts</Link></li>
-        </ul> 
+        </ul>
+        </nav> 
       </aside>
     </>
   )
