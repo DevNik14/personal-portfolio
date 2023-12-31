df1b6841029fb2692f8aa277c245e824a5d5a800
DevNik14 <nykyurchiyski@gmail.com>
2023-12-31 21:34:35 +0200

    Initial React conversion

--- views/contactView.js
+++
@@ -1,7 +0,0 @@
-import {render, html} from 'lit-html';
-
-const contactTemplate = () => html`<h1 class="heading heading-text">Contact</h1>`
-
-export const contactView = (ctx, next) => {
-  ctx.render(contactTemplate());
-}
