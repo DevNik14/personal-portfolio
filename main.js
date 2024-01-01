ca641afd0fd3356b66b585ee6b3335c6617dc715
DevNik14 <nykyurchiyski@gmail.com>
2024-01-01 22:33:09 +0200

    make hash location to reflect active link after page refresh

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const sidebarNivagationElement = document.querySelector('.sidebar');
 const mobileNavigationIcon = document.querySelector('.mobile-navigation-icon');
-const navigationLinkElements = document.querySelectorAll('.nav-link');
+const navigationLinkElements = [...document.querySelectorAll('.nav-link')];
+const currentHashLocation = window.location.hash.split('#')[1];
 
-if(mobileNavigationIcon) {
-  mobileNavigationIcon.addEventListener('click',() => {
+if (mobileNavigationIcon) {
+  mobileNavigationIcon.addEventListener('click', () => {
     sidebarNivagationElement.classList.toggle('to-left');
     mobileNavigationIcon.classList.toggle('open-navigation');
   })
@@ -13,5 +14,20 @@ const clearActiveLinks = () => navigationLinkElements.forEach(link => link.class
 
 navigationLinkElements.forEach(link => link.addEventListener('click', (e) => {
   clearActiveLinks();
-  link.classList.add('active');
+  if (link.textContent.trim() === "</DevNik>") {
+    navigationLinkElements
+      .find(link => link.textContent === "home")
+      .classList
+      .add('active');
+  } else {
+    link.classList.add('active');
+  }
 }))
+
+if (currentHashLocation) {
+  const isSectionSelected = navigationLinkElements
+    .find(link => link.textContent === currentHashLocation);
+  if (isSectionSelected) {
+    isSectionSelected.classList.add('active');
+  }
+}
