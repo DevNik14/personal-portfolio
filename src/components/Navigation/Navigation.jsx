01ff46ccb4141f901a2fe525cf425b170b30b32e
DevNik14 <nykyurchiyski@gmail.com>
2024-01-01 11:39:32 +0200

    add test links

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,42 @@
+import { Link, Element } from 'react-scroll'
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Home</h1>
+    </section>
+  )
+
+}
+
+const About = () => {
+  return (
+    <section>
+      <h1>About</h1>
+    </section>
+  )
+
+}
+
 const Navigation = () => {
   return (
     <>
       <aside className="sidebar">
         <nav className="site-navigation">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contacts</li>
-        </ul>
-        </nav> 
+          <ul>
+            <li><Link to="home" smooth={true} spy={true} duration={250}>Home</Link></li>
+            <li><Link to="about" smooth={true} spy={true} duration={250}>About</Link></li>
+            <li><Link to="projects" smooth={true}>Projects</Link></li>
+            <li><Link to="contacts" smooth={true}>Contacts</Link></li>
+          </ul>
+        </nav>
       </aside>
+      <Element name="home">
+        <Home />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
     </>
   )
 }
