6e9648d073625ead7e0384cb9b8beff353ea328f
DevNik14 <nykyurchiyski@gmail.com>
2024-01-02 21:38:17 +0200

    make hero section elements staggering

--- main.js
+++ main.js
@@ -31,3 +31,14 @@ if (currentHashLocation) {
     isSectionSelected.classList.add('active');
   }
 }
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  })
+})
+
+hiddenElements.forEach(el => observer.observe(el));
