fd54baf8aa44519fbaf23c9fc05df8e9d6c4d905
DevNik14 <nykyurchiyski@gmail.com>
2024-01-10 13:53:38 +0200

    add "Copied" message

--- main.js
+++ main.js
@@ -131,5 +131,14 @@ copyEmailElement.addEventListener('click', (e) => {
   if (e.target.tagName !== 'A') {
     const emailText = copyEmailElement.parentNode.parentNode.querySelector('a').textContent;
     navigator.clipboard.writeText(emailText);
+    
+    const tooltipElement = document.createElement('span');
+    tooltipElement.textContent = 'Copied';
+    tooltipElement.classList.add('show-copied-message');
+    document.querySelector('.email-info').appendChild(tooltipElement);
+
+    const timeoutId = setTimeout(() => {
+      tooltipElement.remove();
+    }, 2000)
   }
 })
